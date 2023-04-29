cb2a1c9957f382cbc01401a9067d2526b999b756
Kostiantyn-Liapkalo <lynx87@ukr.net>
2023-04-29 06:52:38 +0300

    Create server

---
+++ main.py
@@ -0,0 +1,65 @@
+import platform
+from datetime import datetime, timedelta
+import sys
+import asyncio
+import aiohttp
+import json
+
+#Creation of request url depending on the date.
+def create_url(date):
+
+    day, month, year = date.strftime('%d %m %Y').split()
+    return f'https://api.privatbank.ua/p24api/exchange_rates?json&date={day}.{month}.{year}'
+
+
+async def get_exchange(n=None, lst=None):
+    lst_curr = ['USD', 'EUR']
+    if lst:
+        lst_curr += lst
+
+    if n > 10:
+        n = 10
+
+    # Creating a list of urls for the last n days.
+    urls = [create_url(datetime.now() - timedelta(days=day)) for day in range(n-1, -1, -1)]
+    
+    curr_list = []
+
+    async with aiohttp.ClientSession() as session:
+        for url in urls:
+            exchange_rate_for_a_date = {}
+            try:
+                async with session.get(url) as response:
+                    if response.status == 200:
+                        result = await response.json()
+                        curr = {}
+                        for record in result['exchangeRate']:
+                            if record['currency'] in lst_curr:
+                                curr[record['currency']] = {
+                                    'sale': record['saleRate'], 
+                                    'purchase': record['purchaseRate']
+                                }
+                            exchange_rate_for_a_date[result['date']] = curr
+                        curr_list.append(exchange_rate_for_a_date)
+                    else:
+                        print(f'Error status {response.status} for {url}')
+            except aiohttp.ClientConnectionError as err:
+                print(f'Connection error: {url}', str(err))
+
+        exc = json.dumps(curr_list, ensure_ascii=False, indent=4)
+        return exc
+
+
+if __name__ == "__main__":
+    if platform.system() == 'Windows':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    lst_curr = ['USD', 'EUR']
+    try:
+        n = int(sys.argv[1])
+        if len(sys.argv) > 2:
+            # If additional currencies are entered, we add them to the list of default currencies.
+            lst_curr += sys.argv[2:]
+    except IndexError:
+        n = 1
+    r = asyncio.run(get_exchange(n, lst_curr))
+    print(r)
